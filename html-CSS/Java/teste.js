05bd3c468e3c84c3b5a2d19fcef3b8aa98fa2898
Luis Phillipe <l.phillipe.d@gmail.com>
2022-04-29 16:18:02 -0300

    new branc Java

---
+++ html-CSS/Java/teste.js
@@ -0,0 +1,12 @@
+
+function obterMedia () {
+
+    var a = document.getElementById("a").value;
+    var a = document.getElementById("b").value;
+    var a = document.getElementById("c").value;
+
+    var media = (a + b + c) /3;
+
+    document.getElementById ("resultado").innerHTML = "Resultado"
+
+}
