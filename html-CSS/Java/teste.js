debe72a63f2dba833c61f96926f50ee8ee8e21a2
Luis Phillipe <l.phillipe.d@gmail.com>
2022-04-29 17:46:40 -0300

    atualização java

--- html-CSS/Java/teste.js
+++ html-CSS/Java/teste.js
@@ -1,12 +1,14 @@
 
-function obterMedia () {
+function obterMedia() {
 
-    var a = document.getElementById("a").value;
-    var a = document.getElementById("b").value;
-    var a = document.getElementById("c").value;
+    var a = parseInt(document.getElementById("a").value);
+    var b = parseInt(document.getElementById("b").value);
+    var c = parseInt(document.getElementById("c").value);
 
     var media = (a + b + c) /3;
 
-    document.getElementById ("resultado").innerHTML = "Resultado"
+    document.getElementById ("resultado").innerHTML = "Resultado: " + media;
 
-}
+    console.log (a+b+c);
+    
+    }
