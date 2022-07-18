8a44429f6979a2ff0eba59e58a2060984d903f2f
Luis Phillipe <l.phillipe.d@gmail.com>
2022-07-18 17:59:23 -0300

    atualização do gulp conforme aula

--- Jquery/gulpfile.js
+++ Jquery/gulpfile.js
@@ -1,35 +1,45 @@
+const {parallel} = require('gulp')
 const gulp = require('gulp')
 const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
+const htmlmin = require('gulp-htmlmin')
+const babel = require('gulp-babel') 
 
-function tarefasCSS(cb){
+function tarefasCSS(callback){
 
-    return gulp.src(['./vendor/**/*.css',
+     gulp.src(['./vendor/**/*.css',
                       './css/style.css',
                       './node_modules/@fortawesome/fontawesome-free/css/all.css',
-                      './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css'])
+                      './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css'])             
         .pipe(concat('libs.css'))
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'})) //libs.min.css
         .pipe(gulp.dest('./dist/css'))
 
+        return callback()
 }
 
-function tarefasJS(){
+function tarefasJS(callback){
 
-    return gulp.src(['./vendor/jquery/jquery-3.6.0.min.js',
+     gulp.src(['./vendor/jquery/jquery-3.6.0.min.js',
                     './vendor/bootstrap/js/bootstrap.min.js',
                     './vendor/owl/js/owl.js',
                     './vendor/jquery-mask/jquery.mask.min.js',
                     './vendor/jquery-ui/jquery-ui.min.js',
                     './js/custom.js'
                     ])
+            .pipe(babel({
+                comments: false,
+                presets: ['@babel/env']
+            }))        
             .pipe(concat('libs.js'))
             .pipe(uglify())
             .pipe(rename({suffix: '.min'}))
             .pipe(gulp.dest('./dist/js'))
+
+            return callback()
 }
 
 function tarefasImagem(){
@@ -38,14 +48,30 @@ function tarefasImagem(){
         .pipe(gulp.dest('./dist/images'))
 }
 
+
+
+function tarefasHTML(callback){
+
+             gulp.src('src/*.html')
+          .pipe(htmlmin({ collapseWhitespace: true }))
+          .pipe(gulp.dest('dist'));
+
+          return callback()
+
+}
+
 function tarefasdefault(){
     return  tarefasCSS(),
             tarefasJS(),
-            tarefasImagem()
+            tarefasImagem(),
+            tarefasHTML()
 
 }
 
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
 exports.images = tarefasImagem
-exports.default = tarefasdefault
+exports.html = tarefasHTML
+
+
+exports.default = parallel(tarefasHTML, tarefasJS, tarefasCSS)
