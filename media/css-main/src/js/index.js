03862415b213ea01a97276990f5ee5d4d7336254
Luis Phillipe <l.phillipe.d@gmail.com>
2022-08-13 22:44:21 -0300

    movimentação com Iframe

--- media/css-main/src/js/index.js
+++ media/css-main/src/js/index.js
@@ -1,18 +1,49 @@
 import '../scss/styles.scss'
-// import imageBuilding from '../assets/building.jpg'
-// import imageNotebook from '../assets/notebook.jpg'
 
-// const elemRoot = document.getElementById('root')
-// elemRoot.classList.add('container')
+let times = 0, playY;
 
-// const elemImageBuilding = new Image()
-// elemImageBuilding.src = imageBuilding
+const elemRoot = document.getElementById('root')
+elemRoot.classList.add('container')
 
-// const elemImageNotebook = new Image()
-// elemImageNotebook.src = imageNotebook
+const video = document.querySelector('#iframe')
+const button = document.querySelector('#btn-play')
 
-// document.querySelector('#building')
-//     .appendChild(elemImageBuilding)
+document.querySelector('.fa-pause').style.display = 'none';
 
-// document.querySelector('#notebook')
-//     .appendChild(elemImageNotebook)
+
+const play = (event) =>{
+    if(times == 0){
+        playY= video.src +='?autoplay=1';
+        times = 1;
+        document.querySelector('.fa-play').style.display = 'none';
+        document.querySelector('.fa-pause').style.display = 'block';
+    } else {
+        playY = playY.slice(0, -11); // quantidade de caracteres a ser removida no caso remove ?autoplay=1
+        video.src = playY
+        times = 0;
+        document.querySelector('.fa-play').style.display = 'block';
+        document.querySelector('.fa-pause').style.display = 'none';
+    }
+}
+
+
+
+// video.addEventListener('click', (event)=>{
+//     play(event)
+// })
+// função do youtube é priorizada
+
+button.addEventListener('click', (event)=>{
+    play(event)
+})
+
+
+video.addEventListener('timeupdate', ()=>{
+    let current = (video.currentTime / video.duration) * 100
+    if(video.ended){
+        document.querySelector('.fa-play').style.display = 'block';
+        document.querySelector('.fa-pause').style.display = 'none';
+    }
+
+    document.querySelector('.inner').style.width = `${current}`
+})
